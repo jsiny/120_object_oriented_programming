5f61b293f08dd5d88fd75471811880f9817da622
Juliette Sinibardy <juju.siny@gmail.com>
2019-08-01 14:48:47 +0000

    :sparkles: Finish RPS walk-through"

--- lesson_2/12_rps.rb
+++ lesson_2/12_rps.rb
@@ -1,44 +1,78 @@
+class Move
+  VALUES = %w(rock paper scissors)
+
+  def initialize(value)
+    @value = value
+  end
+
+  def >(other_move)
+    rock?        && other_move.scissors? ||
+      paper?     && other_move.rock?     ||
+      scissors?  && other_move.paper?
+  end
+
+  def <(other_move)
+    rock?       && other_move.paper?    ||
+      paper?    && other_move.scissors? ||
+      scissors? && other_move.rock?
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def to_s
+    @value
+  end
+end
+
 class Player
   attr_accessor :move, :name
 
-  def initialize(player_type = :human)
-    @player_type = player_type
-    @move = nil
+  def initialize
     set_name
   end
+end
 
+class Human < Player
   def set_name
-    self.name = if human?
-                  n = ''
-                  loop do
-                    puts "What's your name?"
-                    n = gets.chomp
-                    break unless n.empty?
-                    puts "Sorry, must enter a value"
-                  end
-                  n
-                else
-                  %w(R2D2 Hal9000 Wall-e Baymax Bender Terminator AVA).sample
-                end
+    n = ''
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, must enter a value"
+    end
+    self.name = n
   end
 
   def choose
-    if human?
-      choice = nil
-      loop do
-        puts 'Please choose rock, paper, or scissors:'
-        choice = gets.chomp
-        break if %w(rock paper scissors).include?(choice)
-        puts 'Sorry, invalid choice.'
-      end
-      self.move = choice
-    else
-      self.move = %w(rock paper scissors).sample
+    choice = nil
+    loop do
+      puts 'Please choose rock, paper, or scissors:'
+      choice = gets.chomp
+      break if Move::VALUES.include?(choice)
+      puts 'Sorry, invalid choice.'
     end
+    self.move = Move.new(choice)
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = %w(R2D2 Hal9000 Wall-e Baymax Bender Terminator AVA).sample
   end
 
-  def human?
-    @player_type == :human
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
   end
 end
 
@@ -46,8 +80,8 @@ class RPSGame
   attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new(:computer)
+    @human = Human.new
+    @computer = Computer.new
   end
 
   def play
@@ -55,11 +89,14 @@ class RPSGame
     loop do
       human.choose
       computer.choose
+      display_moves
       display_winner
       break unless play_again?
     end
     display_goodbye_message
   end
+
+  private
 
   def display_welcome_message
     puts "Welcome to Rock, Paper, Scissors"
@@ -69,23 +106,18 @@ class RPSGame
     puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
   end
 
-  def display_winner
+  def display_moves
     puts "#{human.name} chose #{human.move}."
     puts "#{computer.name} chose #{computer.move}."
+  end
 
-    case human.move
-    when 'rock'
-      puts "It's a tie"             if computer.move == 'rock'
-      puts "#{human.name} won!"     if computer.move == 'scissors'
-      puts "#{computer.name} won!"  if computer.move == 'paper'
-    when 'paper'
-      puts "It's a tie"             if computer.move == 'paper'
-      puts "#{human.name} won!"     if computer.move == 'rock'
-      puts "#{computer.name} won!"  if computer.move == 'scissors'
-    when 'scissors'
-      puts "It's a tie"             if computer.move == 'scissors'
-      puts "#{human.name} won!"     if computer.move == 'paper'
-      puts "#{computer.name} won!"  if computer.move == 'rock'
+  def display_winner
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
     end
   end
 
@@ -99,7 +131,7 @@ class RPSGame
       puts "Sorry, must be y or n."
     end
 
-    answer == 'y' ? true : false
+    answer.downcase == 'y' ? true : false
   end
 end
 
