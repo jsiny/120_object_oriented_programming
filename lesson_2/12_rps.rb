0d9c2e600410a040bb56969919ad976969993d13
Juliette Sinibardy <juju.siny@gmail.com>
2019-08-01 08:37:33 +0000

    :tada: First draft of new OO rock paper scissors game

---
+++ lesson_2/12_rps.rb
@@ -0,0 +1,24 @@
+class Player
+  def initialize
+    # name?
+  end
+
+  def choose
+    # returns a move
+  end
+end
+
+class Move
+  def initialize
+    # a move object can be paper rock or scissors
+  end
+end
+
+class Rule
+  def initialize
+    # unsure about the 'state' of a rule object
+  end
+end
+
+def compare(move1, move2)
+end
