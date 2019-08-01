99dfa36e20699ebafe65f73ad00f07d8052e436a
Juliette Sinibardy <juju.siny@gmail.com>
2019-08-01 08:55:32 +0000

    :sparkles: Add basic RPSGame class

--- lesson_2/12_rps.rb
+++ lesson_2/12_rps.rb
@@ -20,5 +20,32 @@ class Rule
   end
 end
 
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Player.new
+    @computer = Player.new
+  end
+
+  def play
+    display_welcome_message
+    human.choose
+    computer.choose
+    display_winner
+    display_goodbye_message
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
+  end
+end
+
 def compare(move1, move2)
 end
+
+RPSGame.new.play
