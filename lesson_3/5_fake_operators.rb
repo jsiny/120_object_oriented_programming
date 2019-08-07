ec865a8c91b0d2fe4e26a3db83801b0583d01e29
Juliette Sinibardy <juju.siny@gmail.com>
2019-08-07 18:33:57 +0200

    :construction: Add Team & Person classes (WIP)

---
+++ lesson_3/5_fake_operators.rb
@@ -0,0 +1,44 @@
+class Team
+  attr_accessor :name, :members
+
+  def initialize(name)
+    @name = name
+    @members = []
+  end
+
+  def <<(person)
+    members.push person
+  end
+
+  def +(other_team)
+    temp_team = Team.new("Temporary Team")
+    temp_team.members = members + other_team.members
+    temp_team
+  end
+end
+
+class Person
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
+
+cowboys = Team.new("Dallas Cowboys")
+emmitt = Person.new("Emmitt Smith", 46)
+
+p cowboys << emmitt                         # will this work?
+p cowboys.members                           # => [#<Person:0x007fe08c209530>]
+
+cowboys << Person.new("Troy Aikman", 48)
+cowboys << Person.new("Michael Irvin", 49)
+
+niners = Team.new("San Francisco 49ers")
+niners << Person.new("Joe Montana", 59)
+niners << Person.new("Jerry Rice", 52)
+niners << Person.new("Deion Sanders", 47)
+
+dream_team = cowboys + niners
+p dream_team
