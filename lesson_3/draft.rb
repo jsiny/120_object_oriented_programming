7e07444b83130183963d69f874617f25a468a74e
Juliette Sinibardy <juju.siny@gmail.com>
2019-08-07 11:13:59 +0200

    :construction: Remove draft content

--- lesson_3/draft.rb
+++ lesson_3/draft.rb
@@ -1,16 +1,1 @@
-class Cat
-  attr_reader :name
-
-  def initialize(name)
-    @name = name
-  end
-
-  def ==(other_cat)
-    @name == other_cat.name
-  end
-end
-
-kitty = Cat.new("teacup")
-kitten = Cat.new("teacup")
-
-p kitty == kitten # => true
+# clear draft
