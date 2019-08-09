e3a92f5e8bd9b0dbe09f7c6094259ff2bbfa6593
Juliette Sinibardy <juju.siny@gmail.com>
2019-08-09 12:12:06 +0200

    :recycle: Refactor Board class

--- lesson_5/ttt.rb
+++ lesson_5/ttt.rb
@@ -10,15 +10,25 @@ class Board
     reset
   end
 
+  def draw
+    puts "     |     |"
+    puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{@squares[4]}  |  #{@squares[5]}  |  #{@squares[6]}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
+    puts "     |     |"
+  end
+
   def reset
     (1..9).each { |key| @squares[key] = Square.new }
   end
 
-  def get_square_at(key)
-    @squares[key]
-  end
-
-  def set_square_at(key, marker)
+  def []=(key, marker)
     @squares[key].marker = marker
   end
 
@@ -31,27 +41,26 @@ class Board
   end
 
   def someone_won?
-    !!detect_winner
+    !!winning_marker
   end
 
   # returns winning marker or returns nil
-  def detect_winner
+  def winning_marker
     WINNING_LINES.each do |line|
-      if count_human_marker(@squares.values_at(*line)) == 3
-        return Game::HUMAN_MARKER
-      elsif count_computer_marker(@squares.values_at(*line)) == 3
-        return Game::COMPUTER_MARKER
+      squares = @squares.values_at(*line)
+      if three_identical_markers?(squares)
+        return squares.first.marker
       end
     end
     nil
   end
 
-  def count_human_marker(squares)
-    squares.map(&:marker).count(Game::HUMAN_MARKER)
-  end
+  private
 
-  def count_computer_marker(squares)
-    squares.map(&:marker).count(Game::COMPUTER_MARKER)
+  def three_identical_markers?(squares)
+    markers = squares.select(&:marked?).map(&:marker)
+    return false if markers.size != 3
+    markers.min == markers.max
   end
 end
 
@@ -69,6 +78,10 @@ class Square
 
   def unmarked?
     marker == INITIAL_MARKER
+  end
+
+  def marked?
+    marker != INITIAL_MARKER
   end
 end
 
@@ -92,10 +105,12 @@ class Game
   end
 
   def play
+    clear
     display_welcome_message
 
     loop do
-      display_board(false)
+      display_board
+
       loop do
         human_moves
         break if board.someone_won? || board.full?
@@ -103,14 +118,12 @@ class Game
         computer_moves
         break if board.someone_won? || board.full?
 
-        display_board
+        clear_screen_and_display_board
       end
       display_result
       break unless play_again?
-      board.reset
-      system('clear') || system('cls')
-      puts "Let's play again!"
-      puts ' '
+      reset
+      display_play_again_message
     end
 
     display_goodbye_message
@@ -124,17 +137,17 @@ class Game
       break if board.unmarked_keys.include?(square)
       puts "Sorry, that's not a valid choice."
     end
-    board.set_square_at(square, human.marker)
+    board[square] = human.marker
   end
 
   def computer_moves
-    board.set_square_at(board.unmarked_keys.sample, computer.marker)
+    board[board.unmarked_keys.sample] = computer.marker
   end
 
   def display_result
-    display_board
+    clear_screen_and_display_board
 
-    case board.detect_winner
+    case board.winning_marker
     when human.marker then puts 'You won!'
     when computer.marker then puts 'Computer won!'
     else puts "It's a tie!"
@@ -144,14 +157,13 @@ class Game
   def play_again?
     answer = ''
     loop do 
-      puts "Would you like tok play again? (y/n)"
+      puts "Would you like to play again? (y/n)"
       answer = gets.chomp.downcase
       break if %(y n).include? answer
       puts "Sorry, must be y or n"
     end
     answer == 'y'
   end
-
 
   def display_welcome_message
     puts 'Welcome to Tic Tac toe!'
@@ -162,22 +174,30 @@ class Game
     puts 'Thanks for playing Tic Tac Toe! Goodbye!'
   end
 
-  def display_board(clear = true)
-    system('clear') || system('cls') if clear
+  def clear_screen_and_display_board
+    clear
+    display_board
+  end
+
+  def display_board
     puts "You're #{human.marker}. Computer is #{computer.marker}."
     puts ""
-    puts "     |     |"
-    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
-    puts "     |     |"
-    puts "" 
+    board.draw
+    puts ""
+  end
+
+  def reset
+    board.reset
+    clear
+  end
+
+  def display_play_again_message
+    puts "Let's play again!"
+    puts ''
+  end
+
+  def clear
+    system('clear')
   end
 end
 
