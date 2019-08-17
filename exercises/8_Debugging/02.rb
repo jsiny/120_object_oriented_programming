8cd54c079fc16386fdd63f90f2e9610d431b00d2
Juliette Sinibardy <juju.siny@gmail.com>
2019-08-17 16:57:29 +0200

    :sparkles: Add ex 8.1 to 8.6

---
+++ exercises/8_Debugging/02.rb
@@ -0,0 +1,46 @@
+class Animal
+  def initialize(diet, superpower)
+    @diet = diet
+    @superpower = superpower
+  end
+
+  def move
+    puts "I'm moving!"
+  end
+
+  def superpower
+    puts "I can #{@superpower}!"
+  end
+end
+
+class Fish < Animal
+  def move
+    puts "I'm swimming!"
+  end
+end
+
+class Bird < Animal
+end
+
+class FlightlessBird < Bird
+  def move
+    puts "I'm running!"
+  end
+end
+
+class SongBird < Bird
+  def initialize(diet, superpower, song)
+    super(diet, superpower)
+    @song = song
+  end
+
+  def move
+    puts "I'm flying!"
+  end
+end
+
+# Examples
+
+Fish.new(:herbivore, 'breathe underwater')
+FlightlessBird.new(:carnivore, 'drink sea water')
+SongBird.new(:omnivore, 'sing', 'chirp chirrr chirp chirp chirrrr')
