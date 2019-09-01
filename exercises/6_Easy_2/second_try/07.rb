b4d182578a330ba3538208a79b31a6f4c0380ef2
Juliette Sinibardy <juju.siny@gmail.com>
2019-09-01 08:32:05 +0200

    :sparkles: Add ex 6.7 to 6.10, second try

--- exercises/6_Easy_2/second_try/07.rb
+++ exercises/6_Easy_2/second_try/07.rb
@@ -7,11 +7,57 @@ class Owner
   end
 
   def number_of_pets
-    @pets.size
+    pets.size
   end
 
-  def add(pet)
-    @pets << pet
+  def adopt(pet)
+    pets << pet
+  end
+
+  def print_pets
+    pets.each { |pet| puts pet }
+  end
+end
+
+class Pet
+  def initialize(animal, name)
+    @animal = animal
+    @name = name
+  end
+
+  def to_s
+    "a #{@animal} named #{@name}"
+  end
+
+  def self.all_pets
+    ObjectSpace.each_object(Pet).to_a
+  end
+end
+
+class Shelter
+  def initialize
+    @owners = []
+    @unadopted = Pet.all_pets
+  end
+
+  def adopt(owner, pet)
+    owner.adopt(pet)
+    @owners << owner
+    @unadopted.delete(pet)
+  end
+
+  def print_unadopted
+    puts "The Animal Shelter has the following unadopted pets:"
+    @unadopted.each { |pet| puts pet }
+    puts
+  end
+
+  def print_adoptions
+    @owners.uniq.each do |owner|
+      puts "#{owner.name} has adopted the following pets:"
+      owner.print_pets
+      puts
+    end
   end
 end
 
@@ -22,9 +68,13 @@ kennedy      = Pet.new('dog', 'Kennedy')
 sweetie      = Pet.new('parakeet', 'Sweetie Pie')
 molly        = Pet.new('dog', 'Molly')
 chester      = Pet.new('fish', 'Chester')
+pixel        = Pet.new('cat', 'Pixel')
+priska       = Pet.new('dog', 'Priska')
+claude       = Pet.new('cat', 'Claude')
 
 phanson = Owner.new('P Hanson')
 bholmes = Owner.new('B Holmes')
+jsiny   = Owner.new('J Siny')
 
 shelter = Shelter.new
 shelter.adopt(phanson, butterscotch)
@@ -34,9 +84,14 @@ shelter.adopt(bholmes, kennedy)
 shelter.adopt(bholmes, sweetie)
 shelter.adopt(bholmes, molly)
 shelter.adopt(bholmes, chester)
+shelter.adopt(jsiny,   pixel)
+
+shelter.print_unadopted
 shelter.print_adoptions
+
 puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
 puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+puts "#{jsiny.name} has #{jsiny.number_of_pets} adopted pets."
 
 # P Hanson has adopted the following pets:
 # a cat named Butterscotch
